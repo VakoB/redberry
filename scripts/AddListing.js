23b3f1175d18d9dbe78aedfee9608aab56eae594
VakoB-a1qa <vladimer.butkhuzi.1@btu.edu.ge>
2024-09-21 23:11:08 +0400

    modal design + validation on add listing page

--- scripts/AddListing.js
+++ scripts/AddListing.js
@@ -13,6 +13,12 @@ function setupCustomSelect() {
       selected.textContent = savedSelection;
     } else if (selectId === "region-select-items") {
       selected.textContent = "აირჩიე";
+    } else if (selectId === "agent-select-items") {
+      const secondOption = items.querySelector("div:nth-child(2)");
+      if (secondOption) {
+        console.log('mamamaama', secondOption.textContent)
+        selected.textContent = secondOption.textContent;
+      }
     } else {
       const firstOption = items.querySelector("div:first-child");
       if (firstOption) {
@@ -30,11 +36,16 @@ function setupCustomSelect() {
 
     items.querySelectorAll("div").forEach((item) => {
       item.addEventListener("click", async function () {
-        selected.textContent = this.textContent;
+        if (this == document.getElementById("add-agent-option")) {
+          displayModal();
+
+        } else {
+          selected.textContent = this.textContent;
+          localStorage.setItem(`selected-${selectId}`, this.textContent);
+        }
         items.style.display = "none";
         customSelect.classList.remove("select-activated");
 
-        localStorage.setItem(`selected-${selectId}`, this.textContent);
 
         if (selectId === "region-select-items") {
           const regionId = await getRegionIdByName(this.textContent);
@@ -141,10 +152,10 @@ async function citiesDropdownState() {
   if (regionSelected && regionSelected !== "აირჩიე") {
     const regionId = await getRegionIdByName(regionSelected);
     await fillDropdownWithCities(regionId);
-    const cityCustomSelect = document.getElementById("city-custom-select");
+    const cityCustomSelect = document.querySelector(".al-city");
     cityCustomSelect.style.visibility = "visible";
   } else {
-    const cityCustomSelect = document.getElementById("city-custom-select");
+    const cityCustomSelect = document.querySelector(".al-city");
     cityCustomSelect.style.visibility = "hidden";
   }
 }
@@ -162,6 +173,31 @@ async function updateSelectedCity() {
   }
 }
 
+function tradeTypeRadioButtonCheck() {
+  const sellRadio = document.getElementById("rb-sell");
+  const rentRadio = document.getElementById("rb-rent");
+
+  const savedTradeType = localStorage.getItem("selectedTradeType");
+
+  if (savedTradeType === "sell") {
+    sellRadio.checked = true;
+  } else if (savedTradeType === "rent") {
+    rentRadio.checked = true;
+  }
+
+  sellRadio.addEventListener("change", () => {
+    if (sellRadio.checked) {
+      localStorage.setItem("selectedTradeType", "sell");
+    }
+  });
+
+  rentRadio.addEventListener("change", () => {
+    if (rentRadio.checked) {
+      localStorage.setItem("selectedTradeType", "rent");
+    }
+  });
+}
+
 // file handling
 
 function fileHandling() {
@@ -172,7 +208,6 @@ function fileHandling() {
 
   const savedImage = localStorage.getItem("uploadedImage");
   if (savedImage) {
-    console.log("allalala")
     previewContainer.style.backgroundImage = `url(${savedImage})`;
     previewContainer.style.display = "block";
     trashIcon.style.display = "block";
@@ -185,20 +220,11 @@ function fileHandling() {
     });
   });
 
-  // dropZone.addEventListener("dragover", () => {
-  //   dropZone.classList.add("active");
-  // });
-
-  // dropZone.addEventListener("dragleave", () => {
-  //   dropZone.classList.remove("active");
-  // });
-
   dropZone.addEventListener("drop", (event) => {
     console.log("im hereee drop");
 
     const files = event.dataTransfer.files;
     handleFile(files);
-    dropZone.classList.remove("active");
   });
 
   dropZone.addEventListener("change", (event) => {
@@ -210,6 +236,7 @@ function fileHandling() {
 
   function handleFile(file) {
     if (file.type.startsWith("image/") && file.size <= 1048576) {
+      document.getElementById("al-file-messages").style.display = "none";
       const reader = new FileReader();
       reader.onload = (e) => {
         const imageData = e.target.result;
@@ -230,9 +257,400 @@ function fileHandling() {
         localStorage.removeItem("uploadedImage");
       });
     } else {
-      console.error("use image type, < 1mb");
+      document.getElementById("al-file-messages").style.display = "block";
     }
   }
+}
+
+function ifClickedDeletePreviewImage() {
+  const fileInput = document.getElementById("al-upload");
+  const previewContainer = document.getElementById("preview-container");
+  const trashIcon = document.getElementById("trash-icon");
+
+  trashIcon.addEventListener("click", () => {
+    previewContainer.style.backgroundImage = "";
+    previewContainer.style.display = "none";
+    trashIcon.style.display = "none";
+    fileInput.value = "";
+    document.getElementById("al-file-messages").style.display = "block";
+
+    localStorage.removeItem("uploadedImage");
+  });
+}
+
+// validation
+
+function liveValidation() {
+  const addressInput = document.getElementById("al-address-input");
+  const postalIndexInput = document.getElementById("al-postal-index-input");
+  const priceInput = document.getElementById("al-price-input");
+  const areaInput = document.getElementById("al-area-input");
+  const roomsInput = document.getElementById("al-rooms-input");
+  const descriptionInput = document.getElementById("al-text-area");
+
+  addressInput.value = localStorage.getItem("address") || "";
+  postalIndexInput.value = localStorage.getItem("postalIndex") || "";
+  priceInput.value = localStorage.getItem("price") || "";
+  areaInput.value = localStorage.getItem("area") || "";
+  roomsInput.value = localStorage.getItem("rooms") || "";
+  descriptionInput.value = localStorage.getItem("description") || "";
+
+  validateInput(
+    addressInput,
+    postalIndexInput,
+    priceInput,
+    areaInput,
+    roomsInput,
+    descriptionInput
+  );
+
+  addressInput.addEventListener("input", () => {
+    const value = addressInput.value;
+    localStorage.setItem("address", value);
+    validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+  });
+
+  postalIndexInput.addEventListener("input", () => {
+    const value = postalIndexInput.value;
+    localStorage.setItem("postalIndex", value);
+    validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+  });
+
+  priceInput.addEventListener("input", () => {
+    const value = priceInput.value;
+    localStorage.setItem("price", value);
+    validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+  });
+
+  areaInput.addEventListener("input", () => {
+    const value = areaInput.value;
+    localStorage.setItem("area", value);
+    validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+  });
+
+  roomsInput.addEventListener("input", () => {
+    const value = roomsInput.value;
+    localStorage.setItem("rooms", value);
+    validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+  });
+
+  descriptionInput.addEventListener("input", () => {
+    const value = descriptionInput.value;
+    localStorage.setItem("description", value);
+    validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+  });
+}
+
+function validationVisualization(container, input, error = false, message) {
+  container.innerHTML = "";
+  const messageContainer = document.createElement("div");
+  const messageElement = document.createElement("p");
+  const img = document.createElement("img");
+  messageContainer.classList.add("al-validation-message");
+
+  img.alt = "tick icon";
+
+  if (!error) {
+    img.src = "/assets/icons/tick-green-icon.svg";
+    input.style.borderColor = "#808a93";
+    messageElement.style.color = "green";
+  } else {
+    img.src = "/assets/icons/tick-red-icon.svg";
+    input.style.borderColor = "red";
+    messageElement.style.color = "red";
+  }
+
+  messageElement.textContent = message;
+
+  messageContainer.appendChild(img);
+  messageContainer.appendChild(messageElement);
+
+  container.appendChild(messageContainer);
+}
+
+function validateInput(
+  addressInput,
+  postalIndexInput,
+  priceInput,
+  areaInput,
+  roomsInput,
+  descriptionInput
+) {
+  // address
+  let allValid = true;
+  const addressValue = addressInput.value;
+  const addressContainer = document.getElementById("al-address-messages");
+  if (addressValue.length == 0) {
+    allValid = false;
+  } else if (addressValue.length >= 2) {
+    addressContainer.innerHTML = "";
+    validationVisualization(
+      addressContainer,
+      addressInput,
+      false,
+      "მინიმუმ ორი სიმბოლო"
+    );
+    
+  } else {
+    allValid = false;
+    validationVisualization(
+      addressContainer,
+      addressInput,
+      true,
+      "მინიმუმ ორი სიმბოლო"
+    );
+  }
+
+  const postalIndexValue = postalIndexInput.value;
+  const postalIndexContainer = document.getElementById(
+    "al-postal-index-messages"
+  );
+  if (postalIndexValue.length == 0) {
+    allValid = false;
+  } else if (/^\d*\.?\d*$/.test(postalIndexValue)) {
+    validationVisualization(
+      postalIndexContainer,
+      postalIndexInput,
+      false,
+      "მხოლოდ რიცხვები"
+    );
+  } else {
+    allValid = false;
+    validationVisualization(
+      postalIndexContainer,
+      postalIndexInput,
+      true,
+      "მხოლოდ რიცხვები"
+    );
+  }
+
+  // price
+  const priceValue = priceInput.value;
+  const priceContainer = document.getElementById("al-price-messages");
+  if (priceValue.length == 0) {
+    allValid = false;
+  } else if (/^\d*\.?\d*$/.test(priceValue)) {
+    validationVisualization(
+      priceContainer,
+      priceInput,
+      false,
+      "მხოლოდ რიცხვები"
+    );
+  } else {
+    allValid = false;
+
+    validationVisualization(
+      priceContainer,
+      priceInput,
+      true,
+      "მხოლოდ რიცხვები"
+    );
+  }
+
+  const areaValue = areaInput.value;
+  const areaContainer = document.getElementById("al-area-messages");
+  if (areaValue.length == 0) {
+    allValid = false;
+  } else if (/^\d*\.?\d*$/.test(areaValue)) {
+    validationVisualization(areaContainer, areaInput, false, "მხოლოდ რიცხვები");
+  } else {
+    allValid = false;
+
+    validationVisualization(areaContainer, areaInput, true, "მხოლოდ რიცხვები");
+  }
+
+  const roomsValue = roomsInput.value;
+  const roomsContainer = document.getElementById("al-rooms-messages");
+  if (roomsValue.length == 0) {
+    allValid = false;
+  } else if (/^\d+$/.test(roomsValue) && Number.isInteger(Number(roomsValue))) {
+    validationVisualization(
+      roomsContainer,
+      roomsInput,
+      false,
+      "მხოლოდ მთელი რიცხვები"
+    );
+  } else {
+    allValid = false;
+
+    validationVisualization(
+      roomsContainer,
+      roomsInput,
+      true,
+      "მხოლოდ მთელი რიცხვები"
+    );
+  }
+
+  const descriptionValue = descriptionInput.value;
+  const wordCount = descriptionValue.trim().split(" ").length;
+  const descriptionContainer = document.getElementById(
+    "al-description-messages"
+  );
+  if (descriptionValue.length == 0) {
+    allValid = false;
+  } else if (wordCount >= 5) {
+    validationVisualization(
+      descriptionContainer,
+      descriptionInput,
+      false,
+      "მინიმუმ ხუთი სიტყვა"
+    );
+  } else {
+    allValid = false;
+
+    validationVisualization(
+      descriptionContainer,
+      descriptionInput,
+      true,
+      "მინიმუმ ხუთი სიტყვა"
+    );
+  }
+  return allValid;
+  
+}
+
+async function getCityIdFromName(name) {
+  const result = await new SendRequest("cities").getRequest();
+  for (let city of result) {
+    if (city["name"] == name) {
+      return city["id"];
+    }
+  }
+}
+
+async function getAgentIdFromName(name) {
+  const result = await new SendRequest("agents").getRequest();
+  for (let agent of result) {
+    if (agent["name"] == name) {
+      return agent["id"];
+    }
+  }
+}
+
+async function ifSubmitOrCancelButtonIsClicked() {
+  const submitButton = document.querySelector(".al-button-submit");
+  const cancelButton = document.querySelector(".al-button-cancel");
+
+  cancelButton.addEventListener("click", () => {
+    window.location.href = "/";
+  });
+
+  submitButton.addEventListener("click", async () => {
+    const addressInput = document.getElementById("al-address-input");
+    const postalIndexInput = document.getElementById("al-postal-index-input");
+    const priceInput = document.getElementById("al-price-input");
+    const areaInput = document.getElementById("al-area-input");
+    const roomsInput = document.getElementById("al-rooms-input");
+    const descriptionInput = document.getElementById("al-text-area");
+    const citySelectedValue = document.querySelector(".al-city .select-selected");
+    const agentSelectedValue = document.querySelector(".al-agent .select-selected");
+    const checkedRadioButton = document.querySelector('input[name="trading-type"]:checked');
+    const labelText = document.querySelector(`label[for="${checkedRadioButton.id}"]`);
+    const fileInput = document.getElementById("al-upload");
+
+  
+    const isValid = validateInput(
+      addressInput,
+      postalIndexInput,
+      priceInput,
+      areaInput,
+      roomsInput,
+      descriptionInput
+    );
+
+    const cityId = await getCityIdFromName(citySelectedValue.value);
+    const agentId = await getAgentIdFromName(agentSelectedValue.value);
+
+    if (isValid) {
+      const file = fileInput.files[0];
+      console.log(file);
+
+      if (!file) {
+        console.error("Please upload a file.");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("price", priceInput.value);
+      formData.append("zip_code", postalIndexInput.value);
+      formData.append("description", descriptionInput.value);
+      formData.append("area", areaInput.value);
+      formData.append("city_id", cityId);
+      formData.append("address", addressInput.value);
+      formData.append("agent_id", agentId);
+      formData.append("bedrooms", roomsInput.value);
+      formData.append("is_rental", labelText.textContent);
+      formData.append("image", file);
+
+
+      const enstance = new SendRequest("real-estate");
+      const result = await enstance.postRequest(formData);
+      console.log(result);
+      
+    } else {
+      console.error("There are invalid or missing inputs.");
+    }
+  });
+}
+
+function modalCloseButton() {
+  document.querySelector(".modal-submit-button").addEventListener("click", () => {
+    closeModal();
+  })
+
+  document.querySelector(".modal-cancel-button").addEventListener("click", () => {
+    closeModal();
+  })
+}
+
+function displayModal() {
+  document.getElementById("modal").showModal();
+}
+
+function closeModal() {
+  document.getElementById("modal").close();
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -245,4 +663,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   setupCustomSelect();
 
   fileHandling();
+  ifClickedDeletePreviewImage();
+
+  liveValidation();
+
+  ifSubmitOrCancelButtonIsClicked();
+
+  tradeTypeRadioButtonCheck();
+  modalCloseButton();
 });
